049c87ae61102304339fe6958397e56f464666cc
22MateusMrs <218542024@eniac.edu.br>
2025-09-03 10:47:40 -0300

    1.0

---
+++ react/frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import{ BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+// Telas Candidatos
+import Cadastro from "../pages/candidatos/Cadastro";
+import Etapas from "../pages/candidatos/Etapas";
+import InscricaoVaga from "../pages/candidatos/InscricaoVaga";
+import Login from "../pages/candidatos/Login";
+import MinhasCandidaturas from "../pages/candidatos/MinhasCandidaturas";
+import TelaInicial from "../pages/candidatos/TelaInicial";
+
+// Telas Rh
+import CadastroVaga from "../pages/rh/CadastroVaga";
+import DetalheVaga from "../pages/rh/DetalheVaga";
+import GestaoVaga from "../pages/rh/GestaoVaga";
+
+export default function AppRoutes() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          {/* Candidato */}
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/inicio" element={<TelaInicial />} />
+          <Route path="/inscricao/:vagaId" element={<InscricaoVaga />} />
+          <Route path="/candidaturas" element={<MinhasCandidaturas />} />
+          <Route path="/etapas/:vagaId" element={<Etapas />} />
+  
+          {/* RH */}
+          <Route path="/rh/dashboard" element={<Dashboard />} />
+          <Route path="/rh/cadastro-vaga" element={<CadastroVaga />} />
+          <Route path="/rh/vaga/:id" element={<DetalheVaga />} />
+          <Route path="/rh/candidato/:id" element={<GestaoCandidato />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
