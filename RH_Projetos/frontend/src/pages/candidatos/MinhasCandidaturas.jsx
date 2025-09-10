6752ef82d4ee8ccbcd2e6a35b2aa15a7030e6a3c
WalterSouza-E <201622023@eniac.edu.br>
2025-09-10 11:53:02 -0300

    Update MinhasCandidaturas.jsx
    MinhasCandidaturas.jsx

--- RH_Projetos/frontend/src/pages/candidatos/MinhasCandidaturas.jsx
+++ RH_Projetos/frontend/src/pages/candidatos/MinhasCandidaturas.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./MinhasCandidaturas.css";
 
 const MinhasCandidaturas = () => {
-    return  <h1>Minhas Candidaturas</h1>;
+    const candidaturas = JSON.parse(localStorage.getItem("candidaturas")) || [];
+    
+
+    return (
+        <div className="minhas-candidaturas">
+            <h2>Minhas Candidaturas</h2>
+            {candidaturas.length === 0 ? (
+                <p>Você ainda não se candidatou a nenhuma vaga.</p>
+            ) : (
+                candidaturas.map((vaga) => (
+                    <div key={vaga.id} className="vaga-candidatada">
+                        <h3>{vaga.nome}</h3>
+                        <p>{vaga.descricao}</p>
+                        <p><strong>Benefícios:</strong> {vaga.beneficios}</p>
+                        <p><strong>Horário:</strong> {vaga.horario}</p>
+                        <p><strong>Salário:</strong> {vaga.salario}</p>
+                    </div>
+                ))
+            )}
+        </div>
+    );
 };
 
 export default MinhasCandidaturas;
