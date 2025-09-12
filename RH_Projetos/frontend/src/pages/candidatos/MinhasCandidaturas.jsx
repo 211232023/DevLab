3568cf8054fdd5fadec33faded59ce55bec9848e
WalterSouza-E <201622023@eniac.edu.br>
2025-09-12 12:48:59 -0300

    Update MinhasCandidaturas

--- RH_Projetos/frontend/src/pages/candidatos/MinhasCandidaturas.jsx
+++ RH_Projetos/frontend/src/pages/candidatos/MinhasCandidaturas.jsx
@@ -1,33 +1,53 @@
 import React, { useEffect, useState } from "react";
 import "./MinhasCandidaturas.css";
-import "./TelaInicial.jsx"
 
 const MinhasCandidaturas = () => {
-    const [candidaturas, setCandidaturas] = useState([]);
+    const [vagasCandidatadas, setVagasCandidatadas] = useState([]);
 
     useEffect(() => {
+        // Busca as vagas candidatas do localStorage
         const vagas = JSON.parse(localStorage.getItem("candidaturas")) || [];
-        setCandidaturas(vagas);
+        setVagasCandidatadas(vagas);
     }, []);
- 
+
+    const renderVagas = () => {
+        if (vagasCandidatadas.length === 0) {
+            return <p>Você ainda não se candidatou a nenhuma vaga.</p>;
+        }
+        return (
+            <ul>
+                {vagasCandidatadas.map((vaga) => (
+                    <ul className="lista-vagas" key={vaga.nome || vaga.descricao || vaga.requisitos.obrigatorios}>
+                        <strong className="nome-vagas">{vaga.nome || vaga}</strong>
+                        <br />
+                        <br />
+                        <strong>{vaga.descricao || vaga}</strong>
+                        <br />
+                        <br />
+                        <strong> Requisitos obrigatórios:{" "}
+                            {Array.isArray(vaga.requisitos?.obrigatorios)
+                                ? vaga.requisitos.obrigatorios.join(", ")
+                                : vaga.requisitos?.obrigatorios || vaga}.
+                        </strong>
+                        <br />
+                        <br />
+                        <button className="bt-informacao">Ver progresso </button>
+                        <button className="bt-informacao">Ver mais informações sobre a vaga</button>
+                    </ul>
+                ))}
+            </ul>
+        );
+    };
+
+
     return (
         <div className="minhas-candidaturas">
-            <h2>Minhas Candidaturas</h2>
-            {candidaturas.length === 0 ? (
-                <p>Você ainda não se candidatou a nenhuma vaga.</p>
-            ) : (
-                candidaturas.map((vaga) => (
-                    <div key={vaga.id} className="vaga-candidatada">
-                        <h3>{vaga.nome}</h3>
-                        <p>{vaga.descricao}</p>
-                        <p><strong>Benefícios:</strong> {vaga.beneficios}</p>
-                        <p><strong>Horário:</strong> {vaga.horario}</p>
-                        <p><strong>Salário:</strong> {vaga.salario}</p>
-                    </div>
-                ))
-            )}
+            <h2>Minhas candidaturas</h2>
+            {renderVagas()}
+
+
         </div>
-    );
+    )
 };
 
 
