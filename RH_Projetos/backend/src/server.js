ba45f127d3e9357430fce3a75271ab6cdf77c1d2
211232023 <211232023@eniac.edu.br>
2025-09-16 12:07:22 -0300

    a

--- RH_Projetos/backend/src/server.js
+++ RH_Projetos/backend/src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // Carrega as variáveis de ambiente
 const express = require('express');
 const cors = require('cors');
 const candidatoRoutes = require('./routes/candidatoRoutes');
 const vagaRoutes = require('./routes/vagaRoutes');
-const authRoutes = require('./routes/authRoutes'); // Adicione esta linha
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +14,7 @@ app.use(express.json());
 // Rotas
 app.use('/api/candidatos', candidatoRoutes);
 app.use('/api/vagas', vagaRoutes);
-app.use('/api', authRoutes); // Adicione esta linha para usar as rotas de autenticação
+app.use('/api', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
