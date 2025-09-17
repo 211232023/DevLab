c93913c1191955d7ddd551342f75726ae9296203
211232023 <211232023@eniac.edu.br>
2025-09-17 10:09:15 -0300

    button

--- RH_Projetos/frontend/src/components/Button.jsx
+++ RH_Projetos/frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ children, onClick, type = "button" }) => {
+const Button = ({ children, onClick, type = "button", style }) => {
   return (
-    <button className="btn" onClick={onClick} type={type}>
+    <button className="btn" onClick={onClick} type={type} style={style}>
       {children}
     </button>
   );
