330d190d4060587583a6d53e2075082818a22fb7
211232023 <211232023@eniac.edu.br>
2025-09-18 12:26:52 -0300

    vaga

--- RH_Projetos/backend/src/server.js
+++ RH_Projetos/backend/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const vagaRoutes = require('./routes/vagaRoutes');
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
-const candidaturaRoutes = require('./routes/candidaturaRoute'); // Corrigido: Importando a rota de candidatura
+const candidaturaRoutes = require('./routes/candidaturaRoute');
 const fileUpload = require('express-fileupload');
 
 const app = express();
@@ -11,13 +11,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload()); // <-- Mova esta linha para cá
 
 // --- ORGANIZANDO AS ROTAS ---
 app.use('/api/auth', authRoutes);
 app.use('/api/vagas', vagaRoutes);
 app.use('/api/usuarios', usuarioRoutes);
-app.use('/api/candidaturas', candidaturaRoutes); // Corrigido: Usando a rota de candidatura no endpoint correto
-app.use(fileUpload());
+app.use('/api/candidaturas', candidaturaRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
