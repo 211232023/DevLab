52d28893580eae1f1d19a1b6a92d6f8920c10f2f
211232023 <211232023@eniac.edu.br>
2025-09-23 06:49:34 -0300

    a

--- RH_Projetos/frontend/src/App.js
+++ RH_Projetos/frontend/src/App.js
@@ -4,12 +4,12 @@ import Footer from "./components/Footer";
 
 const App = () => {
     return (
-        <>
+        <div className="App">
             <div className="app-content">
                 <AppRoutes />
             </div>
             <Footer />
-        </>
+        </div>
     );
 };
 
