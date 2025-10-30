e22e3c44216c6a52139fd55bc365e0bd944760bd
211232023 <211232023@eniac.edu.br>
2025-10-30 15:01:25 -0300

    cadastro

--- RH_Projetos/frontend/src/components/Input.jsx
+++ RH_Projetos/frontend/src/components/Input.jsx
@@ -1,16 +1,23 @@
-const Input = ({ label, name, type = "text", placeholder, value, onChange }) => {
-  return (
-    <div className="input-group">
-      {label && <label htmlFor={name}>{label}</label>}
-      <input
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+import React from 'react';
+import './Input.css';
+
+// 1. Envolva seu componente com React.forwardRef
+// 2. Adicione 'ref' como o segundo argumento
+const Input = React.forwardRef(({ type, id, name, placeholder, value, onChange, required, disabled, className }, ref) => {
+    return (
+        <input
+            type={type}
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            required={required}
+            disabled={disabled}
+            className={`custom-input ${className || ''}`}
+            ref={ref} // 3. Passe a ref para o elemento <input>
+        />
+    );
+});
 
 export default Input;
